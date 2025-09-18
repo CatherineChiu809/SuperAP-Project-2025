681aa59cb745534febee44ce6d5a68b9f48adfb7
Catherine Chiu <catkaiting@gmail.com>
2025-09-18 10:27:50 -0700

    ahhhhhh

---
+++ atask.js
@@ -0,0 +1,24 @@
+class Animal {
+    constructor(s) {
+        subject = s;
+        clicked = false;
+    }
+    constructor(s,c) {
+        subject = s;
+        clicked = c
+    }
+    get sub(){
+        return this.subject;
+    }
+    get click(){
+        return this.clicked;
+    }
+    set clickPosition(on){
+        if(on = true){
+            clicked = true;
+        }
+        else if(on = false){
+            clicked = false;
+        }
+    }
+}
--- input.js
+++ input.js
@@ -1,5 +1,6 @@
 function displayValue() {
     // Get a reference to the button
+    let todoList = [];
     const subButton = document.getElementById("sub");
 
     // Define the function to execute on click
@@ -7,8 +8,10 @@ function displayValue() {
         event.preventDefault();
         const inputElement = document.getElementById("task");
         const newTask = inputElement.value;
+        const newObject = new atask(newTask);
         const taskList = document.getElementById("taskList");
         taskList.innerHTML += newTask + "<br>";
+        todoList.push(newObject);
         inputElement.value = "";
     }
     // Attach the event listener
